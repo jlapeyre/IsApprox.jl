ad26bdb79931d60fd3e93bb80d10a143e8112196
John Lapeyre <jlapeyre@users.noreply.github.com>
2020-03-05 21:28:33 -0800

    Use descriptive identifier

--- src/applications.jl
+++ src/applications.jl
@@ -1,12 +1,12 @@
 # The call `ishermitian(A::AbstractMatrix, B::AbstractMatrix) lowers
 # to exactly the same code as that in LinearAlgebra
-function ishermitian(A::AbstractMatrix, approx::AbstractApprox=Equal())
+function ishermitian(A::AbstractMatrix, approx_test::AbstractApprox=Equal())
     indsm, indsn = axes(A)
     if indsm != indsn
         return false
     end
     for i = indsn, j = i:last(indsn)
-        if ! isapprox(approx, A[i,j], adjoint(A[j,i]))
+        if ! isapprox(approx_test, A[i,j], adjoint(A[j,i]))
             return false
         end
     end
@@ -14,6 +14,6 @@ function ishermitian(A::AbstractMatrix, approx::AbstractApprox=Equal())
 end
 
 # This uses the isapprox interface, which compares using a norm
-ishermitian(A::AbstractMatrix, approx::Approx) = isapprox(approx, A, adjoint(A))
+ishermitian(A::AbstractMatrix, approx_test::Approx) = isapprox(approx_test, A, adjoint(A))
 
-ishermitian(x::Number, approx::AbstractApprox=Equal()) = isapprox(approx, x, conj(x))
+ishermitian(x::Number, approx_test::AbstractApprox=Equal()) = isapprox(approx_test, x, conj(x))
