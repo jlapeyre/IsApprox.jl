823138eed14367020bf85fbc539d7948667430c0
John Lapeyre <jlapeyre@users.noreply.github.com>
2020-06-14 08:17:39 -0700

    Add tests for isunitary

--- test/runtests.jl
+++ test/runtests.jl
@@ -70,3 +70,26 @@ end
     @test ! IsApprox.isposdef(m3)
     @test IsApprox.isposdef(m3, Approx())
 end
+
+@testset "isunitary" begin
+    s0 = [1 0; 0 1]
+    s1 = [0 1; 1 0]
+    s2 = [0 -im; im 0]
+    s3 = [1 0; 0 -1]
+
+    # merr = rand(0:100, 8,8) * 1e-12
+    errmat = [5.3e-11 9.4e-11 3.5e-11 6.6e-11 8.6e-11 4.9e-11 5.2e-11 1.6e-11; 2.8e-11 3.9e-11 4.0e-11 6.0e-12 3.0999999999999996e-11 2.9e-11 9.7e-11 7.1e-11; 8.0e-11 2.5e-11 8.5e-11 7.2e-11 5.7e-11 4.8e-11 5.8e-11 4.1e-11; 5.5e-11 9.8e-11 4.7e-11 8.7e-11 3.6e-11 9.0e-11 4.3e-11 8.0e-11; 7.0e-11 1.5e-11 2.1e-11 9.8e-11 9.7e-11 2.1e-11 7.6e-11 4.0e-11; 4.8e-11 3.9e-11 9.8e-11 0.0 4.0e-11 4.0e-12 4.0e-11 5.3e-11; 6.8e-11 8.3e-11 4.0e-12 1.5e-11 6.7e-11 3.4e-11 1.0e-11 9.0e-12; 7.0e-11 5.2e-11 2.1999999999999998e-11 7.2e-11 9.0e-12 5.4e-11 4.9e-11 5.3e-11]
+
+    m = kron(s1, s2, s3)
+    merr = m + errmat
+
+    @test isunitary(m)
+    @test isunitary(m, Equal())
+    @test ! isunitary(merr)
+    @test ! isunitary(merr, Equal())
+    @test isunitary(merr, Approx())
+    @test ! isunitary(merr, EachApprox())
+    @test ! isunitary(merr, EachApprox(atol=1e-10))
+    @test isunitary(merr, EachApprox(atol=5e-10))
+    @test ! isunitary(merr, Approx(atol=1e-10))
+end
